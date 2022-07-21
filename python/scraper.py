97b1213e62e9b7af7b2487dc06784e830a3da74e
Yosafat <yosafat7054@upi.edu>
2022-07-21 17:54:38 +0700

    python scraper v.2 - formatting, all records retrieved

--- python/scraper.py
+++ python/scraper.py
@@ -1,4 +1,9 @@
 import scrapy
+import re
+
+
+# logging.getLogger('scrapy').propagate = False
+
 # data yang akan diambil
 #     nama
 #     dept
@@ -10,33 +15,35 @@ import scrapy
 #     Affil Score 3Yr
 #     Affil Score
 
-class QuotesSpider(scrapy.Spider):
+class SINTASpider(scrapy.Spider):
     name = "sinta-upi"
 
     def start_requests(self):
         
-        # for urls in 
-        urls = [
-            'https://sinta.kemdikbud.go.id/affiliations/authors/414?page=1',
-            'https://sinta.kemdikbud.go.id/affiliations/authors/414?page=2',
-        ]
+        urls = []
+
+        for a in range (1, 174):
+            urll = 'https://sinta.kemdikbud.go.id/affiliations/authors/414?page=' + str(a)
+            urls.append(urll)
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, response):
 
+    def parse(self, response):
         # first index is 4
         for i in range (4, 14):
             for dosen in response.css('body > div > div.col-md-8 > div.content > div > div.au-list-affil.mt-3'):
                 yield {
                     'nama': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.profile-name > a::text').extract(),
-                    'dept': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-dept > a::text').extract(),
-                    'sinta_id': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-id::text').extract(),
-                    'scopus_h_index': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-hindex > span.profile-id.text-warning::text').extract(),
-                    'gs_h_index': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-hindex > span.profile-id.text-success.ml-3::text').extract(),
+                    'dept': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-dept > a::text').extract()[0].strip(),
+                    'sinta_id': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-id::text').get().replace('ID :', '').replace(' ', ''),
+                    'scopus_h_index': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-hindex > span.profile-id.text-warning::text')
+                    .get().replace('Scopus H-Index :', '').replace(' ', ''),
+                    'gs_h_index': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(1) > div.profile-hindex > span.profile-id.text-success.ml-3::text')
+                    .get().replace('GS H-Index :', '').replace(' ', ''),
                     'sinta_3yr_score': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(2) > div > div:nth-child(1) > div.stat-num.text-center::text').extract(),
                     'sinta_score': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(2) > div > div:nth-child(2) > div.stat-num.text-center::text').extract(),
-                    
                     'affil_3yr_score': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(2) > div > div:nth-child(3) > div.stat-num.text-center::text').extract(),
                     'affil_score': dosen.css('div:nth-child(' + str(i) + ') > div > div.col-lg > div.row > div:nth-child(2) > div > div:nth-child(4) > div.stat-num.text-center::text').extract(),
                 } 
